788828d94687372ebd8528340f83d6dab3408616
Ekaterina Volkova <katerinavolkova2609@gmail.com>
2023-12-18 08:49:55 +0300

    fix dependencies

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import _ from "lodash";
-console.log(_.intersection([2, 1], [3, 2]));
-console.log(_.last(["one", "two"]));
-console.log("Hello, Hexlet!");
+import app from './src/index.js'
+
+app();
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import _ from "lodash";
+// console.log(_.intersection([2, 1], [3, 2]));
+export default console.log(_.last(["one", "two"]));
+// console.log("Hello, Hexlet!");
